47456460cbaa8def11410d09771e34f0a021a68f
Igor Temnikov <dev.temnikov@gmail.com>
2020-04-16 18:26:42 +0200

    0

--- calculator.py
+++ calculator.py
@@ -1,6 +1,15 @@
 #простой калькулятор
+#
+from colorama import init
+from colorama import Fore, Back, Style
+
+#use Colorama to make Termcolor work on Windows too
+init()
+
+print( Back.WHITE )
 
 what = input( "Что делаем? (+, -,): " )
+
 
 a = float(input("Введите первое число: "))
 b = float(input("Введите второй число: "))
